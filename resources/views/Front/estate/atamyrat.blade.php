8064cb2d7829f401e99c29231d71f037de7a0ef5
Atamyrat2005 <komputeratamyrat@gmail.com>
2022-11-26 11:56:40 +0500

    required

--- resources/views/Front/estate/atamyrat.blade.php
+++ resources/views/Front/estate/atamyrat.blade.php
@@ -16,8 +16,14 @@
 </head>
 
 <body style="background:#111; color:#fff;">
-    <h1 class="fw-bold text-center py-3" data-aos="zoom-in" data-duration="3000">Atamyrat Shukurov</h1>
-
+    <h1 class="fw-bold text-center py-3" data-aos="zoom-in-down" data-aos-duration="2000">Atamyrat Shukurov</h1>
+    <div class=" fs-3 d-flex justify-content-center">
+        <a href="https://github.com/Atamyrat2005" data-aos="zoom-in-up" data-aos-duration="600"><i class="bi bi-github text-white px-3"></i></a>
+        <a href="https://t.me/programmist_hacker" data-aos="zoom-in-up" data-aos-duration="1200"><i class="bi bi-telegram text-white px-3"></i></a>
+        <a href="https://www.instagram.com/sukurow_atamyrat" data-aos="zoom-in-up" data-aos-duration="1800"><i class="bi bi-instagram text-white px-3"></i></a>
+        <a href="mailto:komputeratamyrat@gmail.com" data-aos="zoom-in-up" data-aos-duration="2400"><i class="bi bi-envelope text-white px-3"></i></a>
+        <a class="px-3 mb-1" href="https://icq.im/turkmen..hacker" data-aos="zoom-in-up" data-aos-duration="3000"><img width="27" height="27" class="img-fluid" src="{{ asset('img/icons/icq.png')}}" alt=""></a>
+    </div>
 </body>
 
 <script>
